789849702e81dd6937b8f477e9a14c180c47be4c
JohannaMarklund <JohannaMarklund@DESKTOP-T5RS9UL>
2024-11-20 09:50:26 +0100

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory
+            {
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost",
+                UserName = Environment.GetEnvironmentVariable("RABBITMQ_USER") ?? "guest",
+                Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? "guest"
+            };
+
+            //var factory = new ConnectionFactory { HostName = "rabbitmq",                         // Establish a connection
+            //    UserName = "user", Password = "password" };
+
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+                                                                                                 // Declare the queue
+            channel.QueueDeclare(queue: "hello", durable: false, 
+                exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+                                                                                                 // Create a synchronous consumer
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received: {message}");
+            };
+
+                                                                                                 // Start consuming messages
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
